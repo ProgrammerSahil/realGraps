18f3b02bb2b29f8a73edbb505cf85d657d38747d
Sahil Udar <udarsahil0@gmail.com>
2025-02-01 22:07:25 +0530

    a landing page for videos

--- app/index.tsx
+++ app/index.tsx
@@ -1,46 +1,66 @@
-import { View, Text, StyleSheet, ImageBackground, Pressable } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      
-      <ImageBackground style={styles.image} source={{uri: 'https://i.pinimg.com/736x/b6/e8/ee/b6e8eec8c3af2542b80e87cbe38dd597.jpg'}} resizeMode='cover'>
-      <View style={styles.overlay}>
+      <ImageBackground
+        style={styles.image}
+        source={{
+          uri: "https://i.pinimg.com/736x/b6/e8/ee/b6e8eec8c3af2542b80e87cbe38dd597.jpg",
+        }}
+        resizeMode="cover"
+      >
+        <View style={styles.overlay}>
           <Text style={styles.heading}>Real Graps</Text>
-
-        <Link href={'/hangmanvdaniels'} asChild><Pressable>
-          <Text>Hangman vs daniels</Text>
-          </Pressable></Link>
-
+        
+        <View style={styles.matches}>
+        <Link href={"/hangmanvdaniels"} asChild>
+          <Pressable>
+            <Text style={styles.individialMatch} >Hangman vs daniels</Text>
+          </Pressable>
+        </Link>
+        </View>
         </View>
       </ImageBackground>
     </View>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
-  heading:{
-    fontSize:70,
-    color:'white',
-    textAlign:'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  heading: {
+    fontSize: 70,
+    color: "white",
+    textAlign: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
     paddingVertical: 10,
-    fontWeight: 'bold',
-    paddingTop:40
+    fontWeight: "bold",
+    paddingTop: 40,
   },
-  container:{
+  matches: {
+    padding: 20
+  },
+  container: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
-  image:{
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
   overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    height:'100%'
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    height: "100%",
   },
-})
-export default App
+  individialMatch: {
+    fontSize: 30
+  }
+});
+export default App;
